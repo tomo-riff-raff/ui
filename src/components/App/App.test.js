e2f9ecb22965404a078d122ae5ff3cfc8494c7be
Leah Riffell <leah.riffell@gmail.com>
2021-01-31 19:46:29 -0700

    Remote previous react library unit test

--- src/components/App/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders app name', () => {
-  render(<App />);
-  const linkElement = screen.getByText('TOMO');
-  expect(linkElement).toBeInTheDocument();
-});
