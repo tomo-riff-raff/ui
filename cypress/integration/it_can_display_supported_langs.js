91f33830b9b5912f5e900ed929c6001ff780f3ec
Leah Riffell <leah.riffell@gmail.com>
2021-02-08 11:08:29 -0700

    Break test into two blocks

--- cypress/integration/it_can_display_supported_langs.js
+++ cypress/integration/it_can_display_supported_langs.js
@@ -1,10 +1,15 @@
 describe('Languages Supported Page', () => {
-  it('Visits the home page', () => {
+  it('can be visited from home page', () => {
     cy.visit('/')
 
     cy.contains('Languages supported').click()
 
     cy.url().should('include', '/languages')
+  })
+
+  it('displays currently supported languages', () => {
+    cy.visit('/languages')
+
     cy.contains('English')
     cy.contains('Japanese')
   })
