9a1970a0a7149665d94453f69f006c0bdfa0494f
Leah Riffell <leah.riffell@gmail.com>
2021-02-21 15:12:29 -0500

    Remove test for username on login/logout

--- cypress/integration/it_can_log_user_in_and_out.js
+++ cypress/integration/it_can_log_user_in_and_out.js
@@ -1,10 +1,9 @@
-describe('Languages Supported Page', () => {
+describe('Login / logout functionality', () => {
   it('can login user', () => {
     cy.visit('/')
 
     cy.contains('login').click()
 
-    cy.contains('Logged in as: LoganRools')
     cy.contains('logout')
   })
 
@@ -14,7 +13,6 @@ describe('Languages Supported Page', () => {
     cy.contains('login').click()
     cy.contains('logout').click()
 
-    cy.contains('Logged in as: LoganRools').should('not.exist')
     cy.contains('login')
   })
 })
