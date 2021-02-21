4eff15ade204b6f7d09b815cb926ce80a7a9ee41
Leah Riffell <leah.riffell@gmail.com>
2021-02-21 15:13:15 -0500

    Add user context to dashboard component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,24 @@
 import React, { useContext } from "react";
+import { gql, useQuery } from '@apollo/client';
+import { UserContext } from '../../UserContext';
 import Pairings from '../Pairings/Pairings';
 
+const GET_USER = gql`
+  {
+    getUser(id: "1") {
+      username
+    }
+  }
+`;
+
 const Dashboard = () => {
+  const { user, setUser } = useContext(UserContext);
+  const { loading, error, data } = useQuery(GET_USER);
+  
   return (
     <header>
       <h1>Dashboard</h1>
+      <p class="explanatory-text">Logged in as: {data && data.getUser.username}</p>
       <Pairings />
     </header>
   )
