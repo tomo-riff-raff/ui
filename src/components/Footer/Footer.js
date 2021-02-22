b107ce53ee2f9f3fa5b9f43021e3f1ac8c00c4a7
Leah Riffell <leah.riffell@gmail.com>
2021-02-21 18:25:25 -0700

    Remove ul from footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,14 +3,8 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer>
-      <ul>  
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/languages">Languages supported</Link>
-        </li>
-      </ul>
+      <Link to="/">Home</Link>
+      <Link to="/languages">Languages supported</Link>
     </footer>
   )
 }
