bdc74fba5d734139cf43d4708212aa6de5727ebe
Logan Riffell <lkriffell@gmail.com>
2021-03-11 15:35:02 -0500

    Update login component to show form. Nothing happens on form submit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,49 @@
-export const login = async () => {
-  return {
-    id: 1
-  };
-};
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { gql, useQuery } from '@apollo/client';
+import { UserContext } from '../../UserContext';
+
+export const LOGIN = gql`
+query ($email: String!, $password: String!){
+    login(email: $email password: $password) {
+      id
+    }
+  }
+`;
+
+
+
+const LoginForm = () => {
+  const { user, setUser } = useContext(UserContext);
+  const { loading, error, data } = useQuery(LOGIN);
+
+  return (
+    <form>
+      <input
+        className="email"
+        placeholder="email"
+      />
+      <input
+        className="password"
+        placeholder="password"
+      />
+      <button 
+        onClick={() => {
+          setUser(data)
+          {user.id ? (
+            <div>
+              <p>Your email/password combination was incorrect</p>
+              <Link to="/login">Try Again</Link>
+            </div>
+          ) : (
+            <p>Welcome, {user.username}</p>
+          )}
+        }}
+      >
+        Login
+      </button>
+    </form>
+  );
+}
+
+export default LoginForm
